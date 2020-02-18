419386d4aa20094d748e421202d877cae5db50fa
nickubed <nhpquandt@gmail.com>
2020-02-18 13:21:19 -0800

    fetch response functioning

--- src/content/pages/Search.js
+++ src/content/pages/Search.js
@@ -1,34 +1,30 @@
 import React, { useState } from 'react'
 
-const Search = props => {
+const Search = () => {
   // Declare and initialize state variables
     let [query, setQuery] = useState('')
-    
+
+    console.log(query)
     const handleSubmit = e => {
         e.preventDefault()
         let token = localStorage.getItem('userToken')
-        // let q = {
-        //     query
-        // }
-        fetch(`${process.env.REACT_APP_SERVER_URL}/shows`, {
+        fetch(`${process.env.REACT_APP_SERVER_URL}/shows/${query}`, {
             method: 'GET',
-            // body: JSON.stringify(q),
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': `Bearer ${token}`
             }
-            // .then(response => response.json())
-            // .then(result => {
-            //     // Reset the state
-            //     setQuery('')
-            //     console.log(result)
-            // })
-            // .catch(err => {
-            //     console.log('Error Posting', err)
-            // })
         })
+            .then(response => response.json())
+                .then(result => {
+                    // Reset the state
+                    setQuery('')
+                    console.log(result)
+                })
+                .catch(err => {
+                    console.log('Error Getting', err)
+                })
     }
-
     return (
         <div className="search">
             <h2>Search</h2>
