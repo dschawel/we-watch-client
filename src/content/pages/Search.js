dc9acbc225fc671cdc82703701f75e332cc85914
dschawel <david.schawel@gmail.com>
2020-02-19 15:27:48 -0800

    lots going to data

--- src/content/pages/Search.js
+++ src/content/pages/Search.js
@@ -6,7 +6,11 @@ const Search = () => {
   // Declare and initialize state variables
     let [query, setQuery] = useState('')
     let [movieList, setMovieList] = useState([])
-    let [movie, setMovie] = useState('')
+    let [title, setTitle] = useState('')
+    let [type, setType] = useState('')
+    let [year, setYear] = useState('')
+    let [poster, setPoster] = useState('')
+    let [movieArr, setMovieArr] = useState([])
 
     console.log(query)
     const handleSubmit = e => {
@@ -33,30 +37,39 @@ const Search = () => {
         console.log('List:', movieList)
     }
 
-    const addToQueue = e => {
-        e.preventDefault()
-        let token = localStorage.getItem('userToken')
-        fetch(`${process.env.REACT_APP_SERVER_URL}/shows`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-                }
-            })
-            .then(response => response.json())
-                .then(result => {
-                    // setMovie(movie)
-                    console.log('Posting show to user?')
-                    console.log('Added to movie queue', movie)
-                })
-            .catch(err => {
-                console.log('line 19 error', err)
-            })
-    }
+    // const addToQueue = e => {
+    //     console.log(e)
+    //     e.preventDefault()
+    //     let token = localStorage.getItem('userToken')
+    //     let data = {
+    //         movie: e.target.key
+    //     }
+    //     fetch(`${process.env.REACT_APP_SERVER_URL}/shows`, {
+    //         method: 'POST',
+    //         body: JSON.stringify(data),
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //             'Authorization': `Bearer ${token}`
+    //             }
+    //         })
+    //         .then(response => response.json())
+    //         .then(result => {
+    //             // setMovie(movie)
+    //             setTitle('')
+    //             setType('')
+    //             setYear('')
+    //             setPoster('')
+    //             console.log('Posting show to user?', result)
+    //         })
+    //     .catch(err => {
+    //         console.log('line 19 error', err)
+    //     })
+    // }
 
     let list;
     if(movieList){
         list = movieList.map((movie, i) => {
+            console.log(i)
             const CARD_STYLE = {
                 'backgroundImage': 'url(' + movie.Poster + ')',
                 'backgroundSize': 'cover',
@@ -64,10 +77,35 @@ const Search = () => {
                 };
             console.log(movie)
             return(
+                
                 <div className='movie' key={i} style={CARD_STYLE}>
                     <p className='movieTitle'>{movie.Title}</p>
                     <p>{movie.Year}</p>
-                    <button type="submit" onClick={addToQueue} onChange={e => setMovie(e.target.value)}>Add to Queue</button>
+                    <button type="submit" name={i} onClick={(e)=>{
+                        e.preventDefault()
+                        setMovieArr(movieList[e.currentTarget.name])
+                        console.log(`Fresh list comin in hot`, movieArr)
+                        let token = localStorage.getItem('userToken')
+                        let data = {
+                            movie: movieList[e.currentTarget.name]
+                        }
+                        console.log('🤮🤮🤮🤮🤮', data)
+                        fetch(`${process.env.REACT_APP_SERVER_URL}/shows`, {
+                            method: 'POST',
+                            body: JSON.stringify(data),
+                            headers: {
+                                'Content-Type': 'application/json',
+                                'Authorization': `Bearer ${token}`
+                                }
+                            })
+                            .then(response => response.json())
+                            .then(result => {
+                                console.log('Posting show to user?', result)
+                            })
+                        .catch(err => {
+                            console.log('line 110 error', err)
+                        })
+                    }}>Add to Queue</button>
                 </div>
             )
         })
@@ -86,11 +124,11 @@ const Search = () => {
                 </div>
             </form>
             <div className="displayMovies">
-            <div className="cardContainer">
-                <ReactCardCarousel autoplay={ true } autoplay_speed={ 15000 }>
-                    {list}
-                </ReactCardCarousel>
-            </div>
+                <div className="cardContainer">
+                    <ReactCardCarousel autoplay={ true } autoplay_speed={ 15000 }>
+                        {list}
+                    </ReactCardCarousel>
+                </div>
             </div>
         </div>
     )
@@ -98,17 +136,5 @@ const Search = () => {
 
 export default Search
 
-// static get CARD_STYLE() {
-//     return {
-//     height: '200px',
-//     width: '200px',
-//     paddingTop: '80px',
-//     textAlign: 'center',
-//     background: '#52C0F5',
-//     color: '#FFF',
-//     fontSize: '12px',
-//     textTransform: 'uppercase',
-//     borderRadius: '10px',
-//     };
-// }
+
 
