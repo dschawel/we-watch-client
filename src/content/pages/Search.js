55ad037eaaadc17f191d667cb0fdb006744e5e23
dschawel <david.schawel@gmail.com>
2020-02-19 15:37:13 -0800

    Got button to post data

--- src/content/pages/Search.js
+++ src/content/pages/Search.js
@@ -6,10 +6,6 @@ const Search = () => {
   // Declare and initialize state variables
     let [query, setQuery] = useState('')
     let [movieList, setMovieList] = useState([])
-    let [title, setTitle] = useState('')
-    let [type, setType] = useState('')
-    let [year, setYear] = useState('')
-    let [poster, setPoster] = useState('')
     let [movieArr, setMovieArr] = useState([])
 
     console.log(query)
@@ -37,35 +33,6 @@ const Search = () => {
         console.log('List:', movieList)
     }
 
-    // const addToQueue = e => {
-    //     console.log(e)
-    //     e.preventDefault()
-    //     let token = localStorage.getItem('userToken')
-    //     let data = {
-    //         movie: e.target.key
-    //     }
-    //     fetch(`${process.env.REACT_APP_SERVER_URL}/shows`, {
-    //         method: 'POST',
-    //         body: JSON.stringify(data),
-    //         headers: {
-    //             'Content-Type': 'application/json',
-    //             'Authorization': `Bearer ${token}`
-    //             }
-    //         })
-    //         .then(response => response.json())
-    //         .then(result => {
-    //             // setMovie(movie)
-    //             setTitle('')
-    //             setType('')
-    //             setYear('')
-    //             setPoster('')
-    //             console.log('Posting show to user?', result)
-    //         })
-    //     .catch(err => {
-    //         console.log('line 19 error', err)
-    //     })
-    // }
-
     let list;
     if(movieList){
         list = movieList.map((movie, i) => {
@@ -77,19 +44,19 @@ const Search = () => {
                 };
             console.log(movie)
             return(
-                
                 <div className='movie' key={i} style={CARD_STYLE}>
                     <p className='movieTitle'>{movie.Title}</p>
                     <p>{movie.Year}</p>
                     <button type="submit" name={i} onClick={(e)=>{
                         e.preventDefault()
                         setMovieArr(movieList[e.currentTarget.name])
-                        console.log(`Fresh list comin in hot`, movieArr)
                         let token = localStorage.getItem('userToken')
                         let data = {
-                            movie: movieList[e.currentTarget.name]
+                            title: movieList[e.currentTarget.name].Title,
+                            year: movieList[e.currentTarget.name].Year,
+                            poster: movieList[e.currentTarget.name].Poster,
+                            type: movieList[e.currentTarget.name].Type
                         }
-                        console.log('🤮🤮🤮🤮🤮', data)
                         fetch(`${process.env.REACT_APP_SERVER_URL}/shows`, {
                             method: 'POST',
                             body: JSON.stringify(data),
@@ -100,10 +67,10 @@ const Search = () => {
                             })
                             .then(response => response.json())
                             .then(result => {
-                                console.log('Posting show to user?', result)
+                                console.log('Posting show to user!', result)
                             })
                         .catch(err => {
-                            console.log('line 110 error', err)
+                            console.log('line 73, MUCH LESS! error', err)
                         })
                     }}>Add to Queue</button>
                 </div>
